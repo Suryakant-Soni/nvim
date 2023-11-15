4b1b0caaff64b19f63ca180abfe3f13c720f8e73
Suryakant Soni <suryakant.soni@coinbase.com>
2023-11-15 14:45:37 +0530

    initial change to option

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -15,10 +15,10 @@ lsp.nvim_workspace()
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-  ["<C-Space>"] = cmp.mapping.complete(),
+  ['<A-p>'] = cmp.mapping.select_prev_item(cmp_select),
+  ['<A-n>'] = cmp.mapping.select_next_item(cmp_select),
+  ['<A-y>'] = cmp.mapping.confirm({ select = true }),
+  ["<A-Space>"] = cmp.mapping.complete(),
 })
 
 cmp_mappings['<Tab>'] = nil
